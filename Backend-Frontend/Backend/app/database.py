00f5e61b065ba317f8e342fce35627fb262f9d21
Leyder Jaimes <julsgardfen@gmail.com>
2024-12-07 08:22:05 -0600

    Agregar archivo database.py a la carpeta app

---
+++ Backend-Frontend/Backend/app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import URL, create_engine
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker 
+
+from sqlalchemy.engine import URL
+
+
+DATABASE_URL = "postgresql+psycopg2://postgres:Kush534@localhost:5432/zoo"
+#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:lamus.1234*@localhost:5432/zoologico"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()   
